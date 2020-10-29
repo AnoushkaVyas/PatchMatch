2f0cb5aab5165fb9a47f807c87b35897482e2550
kharyal <chaitanyajee@gmail.com>
2020-10-29 08:03:04 +0530

    normal patchmatch up and running

--- src/patchmatch.py
+++ src/patchmatch.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import os, argparse
 from helper_functions import *
 from copy import deepcopy
+from tqdm import tqdm
 
 
 '''
@@ -64,7 +65,7 @@ class PatchMatch(object):
         
         self.random_init(image, image_2)
         is_even = False
-        for iteration in range(self.iterations):
+        for iteration in tqdm(range(self.iterations)):
             
             if iteration%2 == 0:
                 is_even = True
@@ -91,7 +92,7 @@ class PatchMatch(object):
                         [patch_index[0], patch_index[1]+1]])
 
             for index in range(len(indices)-1,-1,-1):
-                if indices[index][0]+1 >= image.shape[0]-self.patch_size or indices[index][1]+1 >= image.shape[1]-self.patch_size:
+                if indices[index][0] >= image.shape[0]-self.patch_size or indices[index][1] >= image.shape[1]-self.patch_size:
                     indices.pop(index)
             
         else:
@@ -100,20 +101,21 @@ class PatchMatch(object):
                         [patch_index[0], patch_index[1]-1]])
 
             for index in range(len(indices)-1,-1,-1):
-                if indices[index][0]-1 < 0 or indices[index][1]-1 < 0:
+                if indices[index][0] < 0 or indices[index][1] < 0:
                     indices.pop(index)
 
-        min_dist = self.nearest_patch_distance[patch_index[0], patch_index[1]]
-        min_loc = deepcopy(patch_index)
+        min_dist = deepcopy(self.nearest_patch_distance[patch_index[0], patch_index[1]])
+        min_loc = deepcopy(self.nearest_patch_location[patch_index[0],patch_index[1]])
             
         for index in indices:
+            # print(self.nearest_patch_location[index[0], index[1]][0])
             dist = self.calulate_distance(
                 image[patch_index[0]:patch_index[0]+self.patch_size, patch_index[1]:patch_index[1]+self.patch_size],
-                image2[index[0]:index[0]+self.patch_size, index[1]:index[1]+self.patch_size]
+                image2[self.nearest_patch_location[index[0], index[1]][0]:self.nearest_patch_location[index[0], index[1]][0]+self.patch_size, self.nearest_patch_location[index[0], index[1]][1]:self.nearest_patch_location[index[0], index[1]][1]+self.patch_size]
             )
             if dist<min_dist:
                 min_dist = dist
-                min_loc = deepcopy(index)
+                min_loc = deepcopy(self.nearest_patch_location[index[0],index[1]])
             
         self.nearest_patch_distance[patch_index[0], patch_index[1]] = min_dist
         self.nearest_patch_location[patch_index[0],patch_index[1],:] = np.array(min_loc)    
@@ -123,26 +125,31 @@ class PatchMatch(object):
         '''
         Random search step of patch match
         '''
-        Ri = np.random.uniform(-1,1,(1,2))
-        random_search_magnitude = np.max(image.shape)*alpha
-        random_search_distance = np.ceil(random_search_magnitude*Ri)
-        current_nearest_patch_location = deepcopy(self.nearest_patch_location[patch_index])
-        current_nearest_patch_distance = deepcopy(self.nearest_patch_distance[patch_index])
+        patch_index = np.array(patch_index)
+        Ri = np.random.uniform(-1,1,(1,2)); Ri = Ri[0]
+        random_search_magnitude = np.max(image2.shape)*alpha
+        random_search_distance = np.ceil(random_search_magnitude*Ri).astype(int)
+        current_nearest_patch_location = deepcopy(self.nearest_patch_location[patch_index[0], patch_index[1]])
+        current_nearest_patch_distance = deepcopy(self.nearest_patch_distance[patch_index[0], patch_index[1]])
 
-        while any(random_search_distance>1):
-            if (current_nearest_patch_location[0]+random_search_distance[0])>image.shape[0] - self.patch_size) or (current_nearest_patch_location[1]+random_search_distance[1])>image.shape[1] - self.patch_size) or (current_nearest_patch_location[0]+random_search_distance[0])<0) or (current_nearest_patch_location[1]+random_search_distance[1])<0):
+        while random_search_distance[0]>1 or random_search_distance[1]>1:
+            if ((current_nearest_patch_location[0]+random_search_distance[0])>image.shape[0] - self.patch_size) or ((current_nearest_patch_location[1]+random_search_distance[1])>image.shape[1] - self.patch_size) or ((current_nearest_patch_location[0]+random_search_distance[0])<0) or ((current_nearest_patch_location[1]+random_search_distance[1])<0):
                 random_search_magnitude = random_search_magnitude*alpha
-                Ri = np.random.uniform(-1,1,(1,2))
-                random_search_distance = np.ceil(random_search_magnitude*Ri)
+                Ri = np.random.uniform(-1,1,(1,2)); Ri = Ri[0]
+                random_search_distance = np.ceil(random_search_magnitude*Ri).astype(int)
                 continue
 
             dist = self.calulate_distance(
                 image[patch_index[0]:patch_index[0]+self.patch_size, patch_index[1]:patch_index[1]+self.patch_size],
                 image2[current_nearest_patch_location[0]+random_search_distance[0]:current_nearest_patch_location[0]+random_search_distance[0]+self.patch_size, current_nearest_patch_location[1]+random_search_distance[1]:current_nearest_patch_location[1]+random_search_distance[1]+self.patch_size]
             )
-            if dist< self.nearest_patch_distance:
-                self.nearest_patch_distance[patch_index] = dist
-                self.nearest_patch_location[patch_index] = current_nearest_patch_location+random_search_distance
+            if dist< self.nearest_patch_distance[patch_index[0], patch_index[1]]:
+                self.nearest_patch_distance[patch_index[0],patch_index[1]] = dist
+                self.nearest_patch_location[patch_index[0],patch_index[1],:] = current_nearest_patch_location+random_search_distance
+            
+            random_search_magnitude = random_search_magnitude*alpha
+            Ri = np.random.uniform(-1,1,(1,2)); Ri = Ri[0]
+            random_search_distance = np.ceil(random_search_magnitude*Ri).astype(int)
 
 if __name__=="__main__":
 

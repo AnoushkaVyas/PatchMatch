f3853dec3951ab2d091866b7234e0fd36eb36b7d
RahulSajnani <rahulsajnani2010@hotmail.com>
2020-11-16 20:41:45 +0530

    Update PatchMatch GUI

--- src/GUI/PatchMatch.m
+++ src/GUI/PatchMatch.m
@@ -161,3 +161,230 @@ if( wbmFlg == 0 && drawFlg >= 0 && draw == 1)
     
     wbmFlg = 0;
 end
+
+% --- Executes on mouse press over figure background, over a disabled or
+% --- inactive control, or over an axes background.
+function figure1_WindowButtonUpFcn(hObject, eventdata, handles)
+% hObject    handle to figure1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global drawFlg;
+global pnSize;
+global imSize;
+global msk;
+global point0;
+global draw;
+
+if(draw == 1)
+    drawFlg = -1;
+    point0 = [];
+elseif(~isempty(point0) && ~isempty(imSize))
+    ps = pnSize / 480 * max([imSize(1),imSize(2)]);
+    point1 = getPixelPosition();
+    if(point1(1) <= imSize(1)&&point1(1) > 0&&point1(2) <= imSize(2)&&point1(2) > 0 && point0(1) <= imSize(1)&&point0(1) > 0&&point0(2) <= imSize(2)&&point0(2) > 0)
+        msk(:,:,draw) = drawLine( msk(:,:,draw), point0, point1, ps, drawFlg );
+        showImageMask(handles.uipanel1);
+        point0 = [];
+    else
+        point0 = [];
+        drawFlg = -1;
+    end
+    
+end
+
+function point = getPixelPosition()
+global imSize;
+
+if( isempty( imSize ) )
+    point = [];
+else
+    cp = get (gca, 'CurrentPoint');
+    cp = cp(1,1:2);
+    row = int32(round( axes2pix(imSize(1), [1 imSize(1)], cp(2)) ));
+    col = int32(round( axes2pix(imSize(2), [1 imSize(2)], cp(1)) ));
+
+    point = [row col];
+end
+
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global filename;
+global dir;
+global img;
+global msk;
+global mskcol;
+global draw;
+
+[d name ext] = fileparts(filename);
+
+msk(:,:,1) = uint8(msk(:,:,1)*255);
+mskimage = double(img).*double(repmat(~msk(:,:,1),1,1,3));
+const = msk(:,:,2)*1+msk(:,:,3)*2+msk(:,:,4)*3;  
+imwrite( uint8(mskimage), [name '_img.png'] );
+imwrite( uint8(msk(:,:,1)), [name '_msk.png'] );
+imwrite( uint8(const), [name '_const.png'] );
+
+cmd = strcat({'./ImageComplete'}, {' '},{name},{'_img.png'},{' '},{name},{'_msk.png'},{' '},{name}, {'_const.png'});
+%[status , cmdout] = system(cmd{1});
+tic;
+system(cmd{1});
+toc;
+
+%axes(handles.uipanel1);
+comp_img = imread('final_out.png');
+
+global maskimage;
+global wbmFlg;
+global point0;
+global drawFlg;
+global imSize;
+global viewMode;
+
+mskcol = [0 0 0];
+wbmFlg = 0;
+point0 = [];
+drawFlg = -1;
+viewMode = 1;
+
+img = comp_img;
+imSize = size(img);
+msk = zeros(imSize(1),imSize(2),4,'uint8');
+maskimage = repmat(img,[1 1 1 4]);
+
+imshow(comp_img);
+
+function [] = showImageMask(handle)
+global img;
+global msk;
+global mskcol;
+global viewMode;
+global draw;
+global maskimage;
+
+
+if( ~isempty(img) )
+    switch( viewMode )
+        case 1
+            maskimage(:,:,:,draw) = genImageMask( single(img), single(msk(:,:,draw)), single(mskcol) );
+        otherwise
+            maskimage(:,:,:,draw) = zeros(size(msk(:,:,draw)));
+    end
+    
+    imshow(uint8(maskimage(:,:,:,draw)));
+end
+
+
+% --- Executes on selection change in popupmenu1.
+
+function popupmenu1_Callback(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns popupmenu1 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from popupmenu1
+global draw;
+global mskcol;
+global maskimage;
+global img;
+
+if(~isempty(img))
+    draw = get(hObject,'Value');
+
+    switch( draw )
+        case 1
+            mskcol = [0 0 0];
+        case 2
+            mskcol = [255 0 0];
+        case 3
+            mskcol = [0 255 0];
+        otherwise
+            mskcol = [0 0 255];
+    end
+
+    imshow(uint8(maskimage(:,:,:,draw)));
+end
+
+
+
+
+% --- Executes during object creation, after setting all properties.
+function popupmenu1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+global draw;
+
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+draw = get(hObject,'Value');
+
+
+% --- Executes on slider movement.
+function slider2_Callback(hObject, eventdata, handles)
+% hObject    handle to slider2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+global pnSize;
+pnSize = 5 + get(hObject,'Value');
+set(handles.text2,'String', pnSize);
+
+
+% --- Executes during object creation, after setting all properties.
+function slider2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to slider2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+
+
+% --- Executes during object creation, after setting all properties.
+function text2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to text2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+global msk;
+global maskimage;
+global img;
+global mskcol;
+msk = [];
+[filename, dir] = uigetfile( '*.*', 'Load Image');
+
+if isequal(filename,0)
+    return;
+end 
+
+tmsk = imread([dir filename]);
+[x y z] = size(tmsk);
+msk = zeros(x,y,4,'uint8');
+msk(:,:,1) = tmsk(:,:,1);
+
+maskimage(:,:,:,1) = genImageMask( single(img), single(msk(:,:,1)), single(mskcol) );
+
+axes(handles.uipanel1);
+imshow(uint8(maskimage(:,:,:,1)));
+
